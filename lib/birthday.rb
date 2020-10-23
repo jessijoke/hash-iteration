93b5c70f6a78077749561198d9c5bb5cec8927e6
jessijoke <jessijoke@yahoo.com>
2020-10-23 00:48:20 -0400

    commit tree 909b3bbc521763cd552e24ff2d1eb868f7d762ab

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -5,7 +5,19 @@
 # }
 
 def happy_birthday(birthday_kids)
-  # add your code snippet here!
-end
+  birthday_kids.each do |kids_name, age|
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  end
+end 
 
+#Didn't understand the 'xit' test thing, thought it meant to skip this. When I got the incomplete I realized that was not correct lol Fixed it B-)
+def age_appropriate_birthday(birthday_kids)
+  birthday_kids.each do |kids_name, age|
+    if age < 12
+      puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+    else
+      puts "You are too old for this."
+    end
+  end
+end 
 
